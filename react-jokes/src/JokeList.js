4b23d89f6e898c820e422ec479eb181ce92e30bf
Scott Campbell <stc9486@gmail.com>
2020-04-01 00:43:12 -0700

    yes

--- react-jokes/src/JokeList.js
+++ react-jokes/src/JokeList.js
@@ -20,6 +20,7 @@ seenJokes = new Set(this.state.jokes.map(j => j.text));
   }
 
   getJokes = async () => {
+    try {
     let jokes = [];
      while //this is the case
             (jokes.length < this.props.numJokesToGet)
@@ -45,6 +46,10 @@ seenJokes = new Set(this.state.jokes.map(j => j.text));
       () =>
         window.localStorage.setItem( "jokes", JSON.stringify(this.state.jokes))
     );
+    } catch (e) {
+      alert(e);
+      this.setState({ loading: false })
+    }
   }
 
   handleClick = () => {
@@ -70,6 +75,7 @@ seenJokes = new Set(this.state.jokes.map(j => j.text));
         </div>
       )
     }
+    let sortJokes = this.state.jokes.sort((a,b) => b.votes - a.votes)
     return (
       <div className="JokeList">
         <div className='JokeList-sidebar'>
@@ -78,11 +84,11 @@ seenJokes = new Set(this.state.jokes.map(j => j.text));
           </h1>
           <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt='nah'/>
           <button className='JokeList-getmore' onClick={this.handleClick}>
-            Fetch Jokes
+            More Jokes
           </button>
         </div>
         <div className="JokeList-jokes">
-          {this.state.jokes.map(j => (
+          {sortJokes.map(j => (
             <Joke 
               key={j.id} 
               votes={j.votes} 
