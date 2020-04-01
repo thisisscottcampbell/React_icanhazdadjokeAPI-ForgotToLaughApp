5ceb81ef8a98b6123322a28ad212225656b4e809
Scott Campbell <stc9486@gmail.com>
2020-03-31 18:29:42 -0700

    while loop

--- react-jokes/src/JokeList.js
+++ react-jokes/src/JokeList.js
@@ -3,10 +3,22 @@ import axios from 'axios';
 
 class JokeList extends Component {
 
+  static defaultProps = {
+    numJokesToGet: 10
+  }
+
+state = { jokes: [] }
+
   componentDidMount = async() => {
-  let res = await axios.get("https://icanhazdadjoke.com/", {headers: { Accept: "application/json" }})
-  console.log(res.data.joke)
-} 
+  let jokes = [];
+  while (jokes.length < this.props.numJokesToGet) {
+    let res = await axios.get("https://icanhazdadjoke.com/", { 
+      headers: { Accept: "application/json" }
+    });
+    jokes.push(res.data.joke)
+  } 
+  this.setState({ jokes: jokes })
+}
 
   render() {
     return (
